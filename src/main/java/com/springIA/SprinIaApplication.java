978f643341f7fb145ac0652273399144c6f02bea
ednaleiden <ednadupont@hotmail.com>
2024-07-15 08:25:01 -0500

    IA generative

---
+++ src/main/java/com/springIA/SprinIaApplication.java
@@ -0,0 +1,13 @@
+package com.springIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinIaApplication.class, args);
+	}
+
+}
